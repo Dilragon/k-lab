56ef6feed5aa0003d05a856f54b0071a4596e426
Dilmurod <102417970+Dilragon@users.noreply.github.com>
2025-07-30 18:02:40 +0500

    Add files via upload

---
+++ react app/App.js
@@ -0,0 +1,27 @@
+import myPhoto from './myPhoto.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div class="profile-card">
+            <img src={myPhoto} alt="My profile picture" class="profile-img" />
+            <h1>John Doe</h1>
+            <h2>Web Developer — Student</h2>
+            <p>I'm 17 years old, from Uzbekistan, Tashkent.</p>
+            <ul class="skills-list">
+                <li>HTML&CSS</li>
+                <li>JavaScript</li>
+                <li>Python</li>
+            </ul>
+        </div>
+        <div class="contact-links">
+            <a href="mailto:dilmurodabdujalilov0305@gmail.com" target="_blank">Email me</a>
+            <a href="https://github.com/Dilragon/k-lab" target="_blank">Github profile</a>
+            <a href="https://t.me/dilragon" target="_blank">Telegram</a>
+        </div>
+    </div>
+  );
+}
+
+export default App;
